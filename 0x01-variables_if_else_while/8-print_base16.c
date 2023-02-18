7592d723c118ce10ed9f31903f9b87a11dbdbdcd
JosVuninga10 <josephvuninganz@gmail.com>
2023-02-18 07:28:41 -0800

    task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,9 +7,12 @@
 int main(void)
 {
 	int n;
+	char letter;
 
-	for (n = 0; n < 16; n++)
-		putchar((n % 16) + '0');
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
+	for (letter = 'a'; letter <=f; letter++);
+		putchar(letter);
 	putchar('\n');
 	return (0);
 }
