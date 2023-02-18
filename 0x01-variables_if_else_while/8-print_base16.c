7149fe43d8eb1acd23719d80801ffba0dfafb1c4
JosVuninga10 <josephvuninganz@gmail.com>
2023-02-18 07:07:49 -0800

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print base 16 numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n
+
+	for (n = 0; n <= 16; n++)
+		putchar((n % 16) + '0');
+	putchar('\n');
+	return (0);
+}
