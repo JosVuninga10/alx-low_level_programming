c9ab832d3cb7f34b2b021502cc920812d9e6e360
JosVuninga10 <josephvuninganz@gmail.com>
2023-02-22 03:03:37 -0800

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print lowercase alphabet
+ * by joseph vuninga
+ * Return: 0
+ */
+int main (void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
