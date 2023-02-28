b0c0994c632c999f9fbbb70f1a865db8c779e8e4
JosVuninga10 <josephvuninganz@gmail.com>
2023-02-28 12:15:26 -0800

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print a string followed by a new line, to stdout
+ * By Joseph Vuninga
+ * Return: 0
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
