185f94150069bdfedc9baa1708503b659538ca7c
JosVuninga10 <josephvuninganz@gmail.com>
2023-03-01 10:08:34 -0800

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _strncat - concatenates two strings
+ * _strcat - concatenates two strings
  * @dest:char
  * @src:char
  * Return:char
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	char *s = dest;
 
