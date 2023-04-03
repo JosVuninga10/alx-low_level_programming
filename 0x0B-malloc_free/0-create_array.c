47077454f1dfb6a692b34c2c0cd567698468b588
JosVuninga10 <josephvuninganz@gmail.com>
2023-04-03 05:23:17 -0700

    malloc free commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,10 +20,8 @@ char *create_array(unsigned int size, char c)
 	array = (char *)malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
-
 	for (i = 0; i < size; i++)
 		array[i] = c;
-
 	return (array);
 }
 
